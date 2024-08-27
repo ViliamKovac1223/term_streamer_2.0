ecb810265450c05068a76d1bb499bd6edd2664ed
ViliamKovac1223 <viliamkovac1223@gmail.com>
2024-08-27 17:51:52 +0200

    Add utils functions

--- src/Utils.cs
+++ src/Utils.cs
@@ -10,4 +10,42 @@ public class Utils
         return fullPath;
     }
 
+    public static string? getFileName(string? path)
+    {
+        if (string.IsNullOrEmpty(path)) return null;
+
+        int lastSlashIndex = path.LastIndexOf('/');
+
+        // If there is no slash in the file path, return entire file name
+        if (lastSlashIndex == -1) return path;
+
+        if (path.Length <= lastSlashIndex + 1) return null;
+
+        return path.Substring(lastSlashIndex + 1);
+    }
+
+    public static string? getDirName(string? path)
+    {
+        if (string.IsNullOrEmpty(path)) return null;
+
+        if (path.ElementAt(path.Length - 1) == '/')
+            path.Remove(path.Length - 1);
+
+        int lastSlashIndex = path.LastIndexOf('/');
+
+        // If there is no slash in the file path, return entire file name
+        if (lastSlashIndex == -1) return path;
+
+        if (path.Length <= lastSlashIndex + 1) return null;
+
+        return path.Substring(lastSlashIndex + 1);
+    }
+
+    public static string normalizePath(string path)
+    {
+        if (path.Last() == '/') return path;
+        path += '/';
+        return path;
+    }
+
 }
